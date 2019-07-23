8a673021a707a7c543c445cce2d820a246fc40e6
JackyShen <mebusw@163.com>
2019-07-23 18:20:28 +0800

    add config to custom component

---
+++ miniprogram/components/canvas.ts
@@ -0,0 +1,36 @@
+// pages/componets/canvas.ts
+
+Component({
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+
+    formatTime(date: Date): string {
+      const year = date.getFullYear()
+      const month = date.getMonth() + 1
+      const day = date.getDate()
+      const hour = date.getHours()
+      const minute = date.getMinutes()
+      const second = date.getSeconds()
+
+      return []
+    }
+
+
+  }
+})
